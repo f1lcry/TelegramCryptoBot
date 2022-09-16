bd38650ad2321087833eaf9ed5d5b13c2cc20b3d
f1lcry <fsr.filipp@gmail.com>
2022-09-16 22:45:53 +0300

    Changed file with image to image in internet to establish getting an image for the review

--- bot.py
+++ bot.py
@@ -11,13 +11,12 @@ from time import sleep
 from datetime import datetime
 from threading import Thread
 
-from PIL import Image
 
 BOT_TOKEN = ""
 CHANNEL_NAME = "@f1lcry"
 news_url = 'https://cryptonews.net/news/top/'
 news_url_key = 'http://www.cryptonews.net'
-market_image = Image.open("rev_img.png")
+market_image = "https://ibb.co/1sJ8m5B"
 bot = telebot.TeleBot(BOT_TOKEN)
 grabber = GrabberArticle(news_url, news_url_key)
 
@@ -55,7 +54,7 @@ def sleep_poster():
         now = time.localtime()
         if now.tm_hour == 5 and now.tm_min == 0:
             print("Creating a message... Progress: 5%")
-            info = "Привет! \U0001F31E Ежедневный обзор рынка: \n\n" + recognize_trend() + "\n\n" + get_prices() + "\n" + get_fng_index() + "\n\n" + get_global_market_info() + "\n\n" + grabber.get_text()
+            info = "Доброе утро! \U0001F31E Ежедневный обзор рынка: \n\n" + recognize_trend() + "\n\n" + get_prices() + "\n" + get_fng_index() + "\n\n" + get_global_market_info() + "\n\n" + grabber.get_text()
             bot.send_photo(CHANNEL_NAME, market_image, caption=info, parse_mode="HTML")
             print('Отправили сообщение!')
 
